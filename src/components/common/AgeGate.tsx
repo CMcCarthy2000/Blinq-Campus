8c617e04c3c7d258b94cf62a7b652f8d57817eb2
Conor McCarthy <168587753+CMcCarthy2000@users.noreply.github.com>
2026-02-23 18:25:12 -0600

    Update AgeGate.tsx
    mainly just edited this so we remember which file does the filter stuff

--- src/components/common/AgeGate.tsx
+++ src/components/common/AgeGate.tsx
@@ -10,6 +10,9 @@ import { Button, Checkbox, Preloader } from "@revoltchat/ui";
 
 import { useApplicationState } from "../../mobx/State";
 import { SECTION_NSFW } from "../../mobx/stores/Layout";
+/**
+We're most likely gonna have to rewrite this to comply with the district rules. Also when testing the new filter lets not use the school network...
+*/
 
 const Base = styled.div`
     display: flex;
